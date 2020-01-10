db5f756dd36a72fb3ad1ea219690862ddd3d2021
rakshassa <rakshassa@gmail.com>
2020-01-10 11:30:01 -0700

    remove apikey from code

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -19,13 +19,14 @@ class SubscriptionsController < ApplicationController
   def tell_mailchimp(email)
     # TODO: use ENV variable: MAILCHIMP_API_KEY  instead of hardcoding in this file
 
-    Gibbon::Request.api_key = "your_api_key"
+    # Gibbon::Request.api_key = "your_api_key"
     Gibbon::Request.timeout = 15
     Gibbon::Request.open_timeout = 15
     Gibbon::Request.symbolize_keys = true
     Gibbon::Request.debug = true
 
-    gibbon = Gibbon::Request.new(api_key: "4c34b7b8c1aeb34fc2a65fd6f10f965f-us4")
+    # gibbon = Gibbon::Request.new(api_key: "4c34b7b8c1aeb34fc2a65fd6f10f965f-us4")
+    gibbon = Gibbon::Request.new
 
     # TODO: these are paged - so paginate iteration
     # lists = gibbon.lists.retrieve
