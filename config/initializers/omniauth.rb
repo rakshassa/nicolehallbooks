b5da2a94d3208dcb27537a66da12d39eccf524a9
Elijah Hall <rakshassa@gmail.com>
2021-02-25 18:51:58 -0700

    removed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '818893941414-k5lo69b25fj379o5g4jhpodogbhggj97.apps.googleusercontent.com', 'Pe5bUmxLCiuJ2U_01tM0Besa'
+  provider :google_oauth2, '818893941414-k5lo69b25fj379o5g4jhpodogbhggj97.apps.googleusercontent.com', 'fake'
 end
